e8907fa3e8bd7a77ff381ad5c37b1c781bb0d1ba
AliDag05 <aliahmedov6925@gmail.com>
2026-08-10 17:51:50 +0300

    Cooldown for shooting

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private float _fireRate = 0.2f;
 
     private float _bulletSpeed = 10.0f;
+    private float _nextFireTime ;
 
     void Start()
     {
@@ -14,20 +16,24 @@ public class PlayerShooting : MonoBehaviour
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0) && _nextFireTime <= Time.time)
         {
             ShootBullet();
+            _nextFireTime = Time.time + _fireRate;
         }
     }
 
     void ShootBullet()
     {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 shootDirection = (mouseWorldPosition - transform.position);
+        
         GameObject spawnedBullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
         Rigidbody2D bulletRb = spawnedBullet.GetComponent<Rigidbody2D>();
 
         if (bulletRb != null)
         {
-            bulletRb.linearVelocity = Vector2.up * _bulletSpeed;
+            bulletRb.linearVelocity = shootDirection.normalized * _bulletSpeed;
         }
     }
 }
