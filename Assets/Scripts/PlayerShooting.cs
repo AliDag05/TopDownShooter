245e936171cf46d6dffcfa1137fb029a2fa336d1
AliDag05 <aliahmedov6925@gmail.com>
2026-08-10 15:16:49 +0300

    PlayerShooting adjustment
    bullets now go up

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private GameObject _bulletPrefab;
-    
+
+    private float _bulletSpeed = 10.0f;
+
     void Start()
     {
         
@@ -20,6 +22,12 @@ public class PlayerShooting : MonoBehaviour
 
     void ShootBullet()
     {
-        Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+        GameObject spawnedBullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D bulletRb = spawnedBullet.GetComponent<Rigidbody2D>();
+
+        if (bulletRb != null)
+        {
+            bulletRb.linearVelocity = Vector2.up * _bulletSpeed;
+        }
     }
 }
